9648d2f93a54bd37262668ccebf57066528239ea
JIG ERA <jig111era@gmail.com>
2026-02-13 14:57:17 +0530

    cors setup

--- server/api/generate.js
+++ server/api/generate.js
@@ -9,7 +9,17 @@ async function ensureLoaded() {
   modelLoaded = true;
 }
 
+function applyCors(res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+}
+
 module.exports = async (req, res) => {
+  applyCors(res);
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
   if (req.method !== "POST") {
     return res.status(405).json({ detail: "Method Not Allowed" });
   }
